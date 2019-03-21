fb8f3188884ad195df6da94baece67822e0cc3d8
winjean <19548901@qq.com>
2019-03-21 10:35:46 +0800

    add listener

--- framework-user/src/main/java/com/winjean/service/impl/UserServiceImpl.java
+++ framework-user/src/main/java/com/winjean/service/impl/UserServiceImpl.java
@@ -13,10 +13,10 @@ import com.winjean.model.response.UserQueryResponse;
 import com.winjean.service.UserService;
 import com.winjean.utils.BeanUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +25,7 @@ import java.util.List;
 @Slf4j
 public class UserServiceImpl extends BaseService implements UserService{
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Override
